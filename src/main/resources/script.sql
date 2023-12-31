7afa338af88e67b088a9093b1cba7ff5dc57e3cb
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2024-01-01 03:05:26 +0500

    Api added

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -8,18 +8,18 @@ VALUES
 
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
 
-INSERT INTO users (created_at, name, password, email, phone, profile_picture_url, device_id, otp, otp_flag, status, location_id)
+INSERT INTO users (created_at, name, password, email, phone, profile_picture_url, device_id, otp, otp_flag, status, location_id, verified)
 VALUES
-    ('2023-01-01 12:00:00', 'Muhammad Tabish', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'muhammadtabish05@gmail.com', '03353183328', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device123', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 1),
-    ('2023-02-02 14:30:00', 'Sham Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'sham.ahmed@email.com', '03331234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device456', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 2),
-    ('2023-03-03 08:45:00', 'Ahmed Raza', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'ahmed.raza@email.com', '03041234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device789', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 3),
-    ('2023-04-04 09:30:00', 'Ayesha Malik', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'ayesha.malik@email.com', '03451234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device101', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 4),
-    ('2023-05-05 11:00:00', 'Kamran Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'kamran.khan@email.com', '03121234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device202', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 5),
-    ('2023-06-06 15:30:00', 'Fatima Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'fatima.khan@email.com', '03451234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device303', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 1),
-    ('2023-07-07 12:45:00', 'Zubair Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'zubair.ahmed@email.com', '03221234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device404', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 2),
-    ('2023-08-08 11:15:00', 'Sara Malik', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'sara.malik@email.com', '03331234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device505', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 3),
-    ('2023-09-09 14:00:00', 'Usman Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'usman.khan@email.com', '03041234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device606', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 4),
-    ('2023-10-10 10:30:00', 'Aisha Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'aisha.ahmed@email.com', '03551234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device707', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 5);
+    ('2023-01-01 12:00:00', 'Muhammad Tabish', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'muhammadtabish05@gmail.com', '03353183328', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device123', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 1, true),
+    ('2023-02-02 14:30:00', 'Sham Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'sham.ahmed@email.com', '03331234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device456', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 2, true),
+    ('2023-03-03 08:45:00', 'Ahmed Raza', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'ahmed.raza@email.com', '03041234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device789', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 3, true),
+    ('2023-04-04 09:30:00', 'Ayesha Malik', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'ayesha.malik@email.com', '03451234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device101', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 4, true),
+    ('2023-05-05 11:00:00', 'Kamran Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'kamran.khan@email.com', '03121234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device202', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 5, true),
+    ('2023-06-06 15:30:00', 'Fatima Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'fatima.khan@email.com', '03451234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device303', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 1, true),
+    ('2023-07-07 12:45:00', 'Zubair Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'zubair.ahmed@email.com', '03221234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device404', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 2, true),
+    ('2023-08-08 11:15:00', 'Sara Malik', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'sara.malik@email.com', '03331234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device505', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 3, false),
+    ('2023-09-09 14:00:00', 'Usman Khan', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'usman.khan@email.com', '03041234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device606', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 4, false),
+    ('2023-10-10 10:30:00', 'Aisha Ahmed', '$2a$10$VIfRcKEbcO6U4WT1oZf/l.xIlAV.Py5cQjOACRqaUNXaSpgtH1lQi', 'aisha.ahmed@email.com', '03551234567', 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg', 'device707', '$2a$10$fy5rnFrxUfZHbqSM3aKGYueg3b53W6SbnlcB6n3UKg6998fMxJkhi', true, true, 5, false);
 
 INSERT INTO `user_roles`(user_id, role_id) VALUES (1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1),(8,1),(9,1),(10,1);
 
@@ -41,13 +41,13 @@ VALUES
     ('2023-04-04 11:15:00', 'Ok carpenter', 10.0, 1.0, 2.6, true, true, true, 9, 3,'094637281','ServiceProvider/ServiceProvider_28/Cnic_2023_12_31_15_02_03.jpg');
 
 
-INSERT INTO booking (created_at, appointment_date, appointment_time, status, user_id, service_provider_id)
+INSERT INTO booking (created_at, appointment_date, appointment_time, status, user_id, service_provider_id, booking_status)
 VALUES
-    ('2023-01-11 14:15:00', '2023-01-20', '12:30:00', true, 1, 7),
-    ('2023-02-12 09:45:00', '2023-02-05', '15:00:00', true, 2, 4),
-    ('2023-03-13 15:30:00', '2023-02-10', '14:00:00', true, 3, 6),
-    ('2023-04-14 18:00:00', '2023-03-15', '16:30:00', true, 4, 8),
-    ('2023-05-15 12:00:00', '2023-03-20', '10:45:00', true, 5, 6);
+    ('2023-01-11 14:15:00', '2023-01-20', '12:30:00', true, 1, 7, "Confirmed"),
+    ('2023-02-12 09:45:00', '2023-02-05', '15:00:00', true, 2, 4, "Confirmed"),
+    ('2023-03-13 15:30:00', '2023-02-10', '14:00:00', true, 3, 6, "Pending"),
+    ('2023-04-14 18:00:00', '2023-03-15', '16:30:00', true, 4, 8, "Pending"),
+    ('2023-05-15 12:00:00', '2023-03-20', '10:45:00', true, 5, 6, "Rejected");
 
 INSERT INTO report_user (created_at, note, status, report_from_user_id, report_to_user_id)
 VALUES
