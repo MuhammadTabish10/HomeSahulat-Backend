b63f9b8f1355d191e1f7c5ab3d0db58aa6ff01bc
Usama Nadeem <usamanadeemparacha@gmail.com>
2024-01-30 23:17:47 +0500

    Request Section

--- Dashboard/assets/js/request.js
+++ Dashboard/assets/js/request.js
@@ -13,6 +13,7 @@ function getAllServiceProvidersByVerify(verified) {
     })
         .then((res) => res.json())
         .then((data) => {
+            debugger
             generateCard(data);
         })
         .catch((err) => {
@@ -33,13 +34,16 @@ function generateCard(employeeData) {
             <div class="card-body pb-5 pt-4">
                 <div class="text-center">
                     <div class="text-right">
-                        <h4><a href="" class="badge badge-primary badge-pill px-3 py-2" onclick="">Approve</a></h4>
-                        <h4><a href="" class="badge badge-primary badge-pill px-3 py-2" onclick="">Reject</a></h4>
+                        <h4><a class="badge badge-primary badge-pill px-3 py-2" onclick="approve(true, ${employee.id})">Approve</a></h4>
+                        <h4><a class="badge badge-primary badge-pill px-3 py-2" onclick="reject(false,${employee.id})">Reject</a></h4>
                     </div>
                     <div class="pt-1 bg-img m-auto"><img src="${employee?.user?.profilePictureUrl}" class="img-fluid h-100" alt="employees-img"></div>
                     <div class="mt-3 employees-contant-inner">
                         <h4 class="mb-1">${employee?.user?.name}</h4>
-                        <h5 class="mb-0 text-muted">${employee?.services?.name}</h5>
+                        <h5 class="my-3 text-muted">${employee?.services?.name}</h5>
+                        <h5 class="my-3 text-muted d-inline">Hourly Price: ${employee?.hourlyPrice}</h5>
+                        <h5 class="my-3 text-muted d-inline px-1">Experience: ${employee?.totalExperience}</h5>
+                        <h5 class="my-3 text-muted d-inline px-1">Rating: ${employee?.totalRating}</h5>
                         <div class="mt-3">
                             <a class="badge badge-pill badge-info-inverse px-3 py-2" onclick="downloadImg(${employee.id})">CNIC</a>
                             <a class="badge badge-pill badge-info-inverse px-3 py-2">Shop No</a>
@@ -84,4 +88,42 @@ function downloadImg(id) {
     const link = document.createElement('a');
     link.href = `http://localhost:8080/api/file/ServiceProvider/ServiceProvider_${10}/Cnic_2023_12_30_15_55_17.png`;
     link.click();
+}
+
+function approve(status, id) {
+    const requestData = { id, verify: status }; // Assuming this is the structure you expect
+    console.log('Request data:', requestData);
+
+    fetch(`${url}/service-provider/${id}/verify/${status}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(requestData),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log('Response data:', data);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+}
+
+
+function reject(status, id) {
+    fetch(`${url}/service-provider/${id}/verify/${status}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
